5fce6fedc4acd99c1c95dcddb33838d9d0f908e2
rajke24 <jarek.andrzejewski99@gmail.com>
2020-07-29 14:12:00 +0200

    add error handling

--- gsrap.py
+++ gsrap.py
@@ -8,12 +8,15 @@ def insert_data(data):
         "https://www.googleapis.com/auth/drive.file",
         "https://www.googleapis.com/auth/drive"
         ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\jarek\OneDrive\Dokumenty\learning\Projects\python\ck_raporty\client_secret.json', scope)
-    client = gspread.authorize(creds)
-    sheet = client.open('CK_raporty').sheet1
-    
-    row_num = 2
-    data['random_sets'] = '\n'.join([f'{no}x komplet - {price} zł' for price, no in data['random_sets'].items() ])
-    data = list(data.values())
-    sheet.insert_row(data, row_num)
+    try:
+        creds = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\jarek\OneDrive\Dokumenty\learning\Projects\python\ck_raporty\client_secret.json', scope)
+        client = gspread.authorize(creds)
+        sheet = client.open('CK_raporty').sheet1
+        
+        row_num = 2
+        data['random_sets'] = '\n'.join([f'{no}x komplet - {price} zł' for price, no in data['random_sets'].items() ])
+        data = list(data.values())
+        sheet.insert_row(data, row_num)
+    except:
+        return False
 
